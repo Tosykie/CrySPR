e8abb00f2a4c26800943164ca181a0f1d99240b0
Tosykie <nw2y47@outlook.com>
2024-04-21 02:34:57 +0800

    Initial commit

---
+++ cryspr/__init__.py
@@ -0,0 +1,14 @@
+# Copyright (c) 2024, Kedar Hippalgaonkar's Materials by Design Lab
+# Distributed under the terms of the MIT License.
+
+"""
+CrySPR is a collection of Python codes for crystal structure pre-relaxation and prediction.
+"""
+
+__author__     = "Wei Nong, Ruiming Zhu, Kedar Hippalgaonkar"
+__copyright__  = "Copyright 2024, Kedar Hippalgaonkar's Materials by Design Lab"
+__version__    =  "0.0.1"
+__maintainer__ = "Wei Nong"
+__email__      = "nong0003@e.ntu.edu.sg"
+__status__     = "Development"
+__date__       = "April 21, 2024"
---
+++ cryspr/wycryst/__ini__.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2024, Kedar Hippalgaonkar's Materials by Design Lab
+# Distributed under the terms of the MIT License.
+
+"""
+WyCryt is a generative design framework for crystal structure generation
+with variational autoencoder and Wyckoff position-based crystal representation.
+"""
