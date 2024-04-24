02c3d82e7876d53eda2f5a7df73722831b0938e2
Tosykie <nw2y47@outlook.com>
2024-04-24 18:04:50 +0800

    Added wrappers for calculators

--- cryspr/calculators/__init__.py
+++ cryspr/calculators/__init__.py
@@ -1,1 +1,53 @@
 """Include calculators"""
+import matgl
+from matgl.ext.ase import PESCalculator
+from matgl.apps.pes import Potential
+
+from mace.calculators import mace_mp, MACECalculator
+
+from chgnet.model.model import CHGNet
+from chgnet.model import CHGNetCalculator as _CHGNetCalculator
+
+class M3GNetCalculator(PESCalculator):
+    """
+    Wrapper of matgl.ext.ase.PESCalculator
+    using eV/A^3 unit for stress and
+    pretrained M3GNet-MP-2021.2.8-PES.
+    """
+    def __init__(
+        self,
+        potential: Potential = matgl.load_model("M3GNet-MP-2021.2.8-PES"),
+        stress_weight: float = 1 / 160.21766208,
+        **kwargs,
+    ):
+        super().__init__(potential=potential, stress_weight=stress_weight, **kwargs)
+
+
+# Override the original MACECalculator
+_MACECalculator: MACECalculator = mace_mp()
+class MaceMPCalculator(_MACECalculator):
+    """
+    Wrapper of mace.calculators.mace_mp function using
+    pretrained model based on the Materials Project, with
+    cpu as compute device and float32 precision.
+    """
+    def __init__(
+        self,
+        model = None,
+        device: str = "cpu",
+        default_dtype: str = "float32",
+        **kwargs,
+    ):
+        super().__init__(model=model, device=device, default_dtype=default_dtype, **kwargs)
+
+class CHGNetCalculator(_CHGNetCalculator):
+    """
+    Wrapper of chgnet.model.CHGNetCalculator with cpu as default device.
+    """
+    def __init__(
+            self,
+            model: CHGNet = None,
+            device: str = "cpu",
+    ):
+        super().__init__(model=model, use_device=device)
+
