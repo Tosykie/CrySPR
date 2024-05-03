251d01d303b66f0fac9664d6dca1a50d5823b28c
Tosykie <nw2y47@outlook.com>
2024-05-03 22:40:31 +0800

    Update search_by_pso

--- cryspr/optimization/global.py
+++ cryspr/optimization/global.py
@@ -1,12 +1,10 @@
 """Global optimization"""
 
+from typing import Callable
 from ase import Atoms
 from pyxtal.symmetry import Group
 from sko.PSO import PSO
 from.local import stepwise_relax
-
-def local_energy(atoms_relaxed: Atoms):
-    return atoms_relaxed.get_potential_energy()
 
 def initialize_search_space():
     """TO-DO: To get the lower and upper boundaries
@@ -19,19 +17,34 @@ def initialize_search_space():
     """
     pass
 
-def search_by_pso(*args, **kwargs):
-    population = 80
-    max_pso_step = 200
-    pso = PSO(func=local_energy,
-              n_dim=5, # the dimension of search space (as defined in the boundary)
+def search_by_pso(objective_func: Callable,
+                  n_dim: int, # dimension of particles, which is the number of variables of objective_func
+                  lower_boundary: list, # lower boundary of search space
+                  upper_boundary: list, # upper boundary of search space
+                  population: int = 80, # population size (number of particles)
+                  inertia_weight: float = 0.8, # PSO ratio in the population for each generation
+                  max_pso_generation: int = 200, # max PSO step
+                  acceleration_constant1: float = 0.5,
+                  acceleration_constant2: float = 0.5,
+                  n_cpus: int = 0, # Number of processes, 0 means use all cpu
+                  verbose=True,
+                  *args,
+                  **kwargs,
+                  ) -> PSO:
+
+    pso = PSO(func=objective_func,
+              n_dim=n_dim,
               pop=population,
-              max_iter=max_pso_step,
-              lb=None,
-              ub=None,
-              w=0.8,
-              c1=0.5,
-              c2=0.5,
-              verbose=True,
+              max_iter=max_pso_generation,
+              lb=lower_boundary,
+              ub=upper_boundary,
+              w=inertia_weight,
+              c1=acceleration_constant1,
+              c2=acceleration_constant2,
+              # n_processes=n_cpus, # only available in dev version of sko
+              verbose=verbose,
+              *args,
+              **kwargs,
               )
     pso.run()
     # print('best_x is ', pso.gbest_x, 'best_y is', pso.gbest_y)
